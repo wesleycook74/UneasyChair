8d057ce61943c3e6f0e2062a261e2cc6e5f1da03
Wesley Cook <wesleycook74@gmail.com>
2017-03-05 18:04:03 -0500

    can make and create tracks from conference

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -10,6 +10,7 @@ class ConferencesController < ApplicationController
   # GET /conferences/1
   # GET /conferences/1.json
   def show
+    @tracks = @conference.tracks
   end
 
   # GET /conferences/new
--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,4 +1,3 @@
 class Conference < ApplicationRecord
 	has_many :tracks
-	has_many :users, through: :tracks
 end
