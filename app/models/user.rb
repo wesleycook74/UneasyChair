bf06b7847cb5c281cef5fd5721efa45f3589898c
sdholcomb <sdholcomb@valdosta.edu>
2017-03-04 23:00:45 -0500

    Added devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-	has_many :tracks, optional: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+	# has_many :tracks, optional: true
 end
