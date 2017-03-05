2b2f2900907babc1698f0cf27a1c33abf61f39c1
sdholcomb <sdholcomb@valdosta.edu>
2017-03-05 17:06:15 -0500

    Edited registration

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   resources :papers
 
+  # devise_for :members, :controllers => { :registrations => "registrations" }
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
