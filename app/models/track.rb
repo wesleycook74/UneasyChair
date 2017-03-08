12aaca7b4d1adbb2dc80a502d18b32395d664032
Wesley Cook <wesleycook74@gmail.com>
2017-03-07 19:08:26 -0500

    added user_roles model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,10 @@
 class Track < ApplicationRecord
 	belongs_to :conference
 	has_many :papers
-	has_many :users
+	has_many :user_roles
+	has_many :users, through: :user_roles
+
+	def add_user(user, role)
+		self.user_roles.build(user_id: user, role: role)
+	end
 end
