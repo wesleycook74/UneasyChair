4c2ab468066a7cc7de442546fcfd4fcb6b09905f
Benjamin Hafer <bchafer.bch@gmail.com>
2017-03-09 13:46:09 -0500

    updated aws.rb in initializers

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 AWS.config(
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :access_key_id => ENV['AKIAJPZLHU63XHW5JN6A'],
+    :secret_access_key => ENV['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']
 )
 
-S3_BUCKET =  AWS::S3.new.buckets[ENV['S3_BUCKET']]
+S3_BUCKET =  AWS::S3.new.buckets[ENV['uneasychairz']]
