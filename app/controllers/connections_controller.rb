23405253a4108a2a0909e848948f6114555a6f49
sdholcomb <ghostholcomb@gmail.com>
2017-03-27 22:06:38 -0400

    Added ugly contacts on user page (to make sure it works)

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -3,11 +3,18 @@ class ConnectionsController < ApplicationController
   def create
     @connection = current_user.connections.build(:contact_id => params[:contact_id])
     if @connection.save
-      flash[:notice] = "Added connection."
+      flash[:notice] = "Added contact."
       redirect_to root_url
     else
-      flash[:notice] = "Unable to add friend."
+      flash[:notice] = "Unable to add contact."
       redirect_to root_url
     end
+  end
+
+  def destroy
+    @connection = current_user.connections.find(params[:id])
+    @connection.destroy
+    flash[:notice] = "Removed contact."
+    redirect_to root_url
   end
 end
