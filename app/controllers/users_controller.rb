315cfbd7b62e473407b2ed5f85b82d803e9a8937
sdholcomb <ghostholcomb@gmail.com>
2017-04-19 17:13:32 -0400

    commit before pull

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @user_roles = @user.user_roles
     @date = @user.created_at
     @date.strftime("%B %d, %Y")
