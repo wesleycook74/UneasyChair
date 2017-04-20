b606a62ceae30d332ce9a53b73868b2d77ac5ae4
sdholcomb <ghostholcomb@gmail.com>
2017-04-20 01:14:07 -0400

    Notification now built upon paper acceptance and notification is extendable to anything

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,7 @@
 module NotificationsHelper
+  def create_notification(user, title, message)
+    notification = user.notifications.build(user_id: :user,:title => title, :message => message )
+      notification.save
+
+  end
 end
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,6 +3,7 @@ class Paper < ApplicationRecord
   belongs_to :user
   has_many :reviews
   validates_presence_of :title, :abstract, :author, :attachment
+  include NotificationsHelper
   mount_uploader :attachment, AttachmentUploader
 
   def automatic_accept
@@ -11,8 +12,10 @@ class Paper < ApplicationRecord
 
       @average_score = self.reviews.sum(:score).to_f / self.reviews.count
       @average_confidence = self.reviews.sum(:confidence).to_f / self.reviews.count
+      @owner = User.find(self.user_id)
       if @average_score > 1.4 && @average_confidence > 2
         self.update_attribute(:accepted, true)
+        create_notification(@owner, 'Your paper has been accepted!', "Congratulations, your paper, #{self.title}, has been accepted")
       end
     end
   end
